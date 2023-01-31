03ad824e26913e21954643e9cf81564b9eddac82
Max Luan <max_luan@foxmail.com>
2023-01-31 21:52:06 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,vue,jsx}"
+    ],
+    darkMode: 'media',
+    theme: {
+        extend: {}
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: []
+}
